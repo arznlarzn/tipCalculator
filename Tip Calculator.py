db7aa16a3b378ca8343200449b1076cf8df9dff6
Larson <larsonjdavis@gmail.com>
2026-04-15 15:44:12 -0600

    Update Tip Calculator.py
    Made several changes- Primarily, added user input for the tip percentage, adjusted window size, and added a border around the answer box to not have empty space.

--- Tip Calculator.py
+++ Tip Calculator.py
@@ -10,18 +10,22 @@ class MyGUI:
 
         self.root = Tk()
         self.root.title('Tip Calculator!')
-        self.root.geometry('600x370')
+        self.root.geometry('600x400')
 
-        self.things = Label(self.root, text='The default value for our tip calculation is 15%.', font=('Times New Roman', 14))
+
+        self.things = Label(self.root, text='The value of the tip, as a percentage, is:', font=('Times New Roman', 14))
         self.things.pack(padx=10,pady=20)
-        self.instruction = Label(self.root, text='Here, enter the total amount of the service used:', font=('Times New Roman', 20))
+        self.tip = Entry(self.root, width=14, font=('Times New Roman', 14))
+        self.tip.insert(0, '15')
+        self.tip.pack(padx=10, pady=10)
+        self.instruction = Label(self.root, text='Here, enter the total amount of the service used:', font=('Times New Roman', 14))
         self.instruction.pack(padx= 10, pady=10)
-        self.textbox = Entry(self.root, width=22, font=('Times New Roman', 16))
+        self.textbox = Entry(self.root, width=22, font=('Times New Roman', 14))
         self.textbox.pack(padx=10, pady=10)
         self.button = Button(self.root, text='Calculate', font=('Times New Roman', 22), command=self.calculate)
         self.button.pack(padx=10,pady=10)
 
-        self.answer = Label(self.root, text= ' ', font=('Times New Roman', 22))
+        self.answer = Label(self.root, bd=2, relief="solid", text=' ', font=('Times New Roman', 22))
         self.answer.pack(padx=10,pady=10)
 
         #this is the instruction for the user- this just demonstrates that there is logic to let the user know if they didn't enter an amount
@@ -39,7 +43,7 @@ class MyGUI:
         if self.textbox.get() == '':
             messagebox.showinfo(title='Wtf?', message='You have to put SOME amount in there!? Geez. *Facepalm*')
         else:
-            total = float(self.textbox.get()) * 1.15
+            total = float(self.textbox.get()) * (float(self.tip.get()) / 100)
             formatted_total = "{:.2f}".format(total)
             self.answer.config(text=formatted_total)
 
